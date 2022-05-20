628cfc52264ead45ebb4d1a361c6fe7516ab5c51
chenhaojie <a18520145493@163.com>
2022-05-20 18:32:19 +0800

    新增模式匹配查询

---
+++ uns-ts/src/utils/module-match.ts
@@ -0,0 +1,18 @@
+/**
+ * 模式匹配
+ *  1.TS同样支持模式匹配，通过匹配规则，找到匹配的部分，提取合适的部分
+ *  2.通过extends对类型参数做匹配，然后使用 infer 声明局部类型变量存储起来，如果匹配则从该局部变量中提取出来，如果不匹配则返回never
+ */
+
+// 例子：type p = Promise<'guang'>; 提取Promise返回值类型
+type GetPromiseValue<T> = T extends Promise<infer K> ? K : never;
+type getPromiseRes = GetPromiseValue<Promise<"guang">>; // 'guang'
+
+/**
+ * 可以将模式匹配应用到数组，字符串，函数，构造器类型等应用中
+ */
+
+// Array类型
+
+
+export {};
