36df4f6cc0638e385a91d78069e1652b77f5147f
chenhaojie <a18520145493@163.com>
2022-05-20 14:46:28 +0800

    使用expect匹配器

--- uns-ts/src/test/sum.test.ts
+++ uns-ts/src/test/sum.test.ts
@@ -1,4 +1,4 @@
 import { sum } from "../utils/sum";
 test("adds 1 + 2 to 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBeGreaterThan(1); //toBeGreaterThan：用于测试比较 输入的值是否小于 expect输入的值 相当于 a > b的关系
 });
