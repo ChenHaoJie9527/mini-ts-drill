12b379b0d884dcd7f3a84a5a57ea63213acb1af8
tourl-ts <18312883857@163.com>
2022-08-18 00:06:14 +0800

    feat: 切换test测试工具

--- uns-ts/src/test/sum.test.ts
+++ uns-ts/src/test/sum.test.ts
@@ -1,4 +1,9 @@
-import { sum } from "../utils/sum";
-test("adds 1 + 2 to 3", () => {
-  expect(sum(1, 2)).toBeGreaterThan(1); //toBeGreaterThan：用于测试比较 输入的值是否小于 expect输入的值 相当于 a > b的关系
-});
+import { describe, expect, it } from "vitest";
+
+describe('sum', () => {
+  it('should sum', () => {
+    const a = 10;
+    const b = 20;
+    expect(a + b).toBe(30);
+  })
+})
